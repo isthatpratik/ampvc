a147cc56f694962a49f0349ff75d42cd522bb184
isthatpratik <pratik.desai18.pd@gmail.com>
2025-03-03 10:16:57 +0530

    Form Submission Changes

--- app/api/chatbot/chat/route.ts
+++ app/api/chatbot/chat/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
     const response = await openai.chat.completions.create({
       model: "gpt-4o",
       messages: [
-        { role: "system", content: "You are FynarAi, an AI VC matchmaking chatbot by Ampersand. Look at the vc_data.json present in the public folder. Match with the parameters. After matching, only show investor names in numbered list format and strictly use proper formatting and spacing. At the end of the list, include the text '[Download Full List]'. Give only information related to VCs, investors, or startups." },
+        { role: "system", content: "You are FynarAi, an AI VC matchmaking chatbot by Ampersand. Look at the vc_data.json present in the public folder. Match with the parameters. After matching, only show top three investor names in numbered list format and strictly use proper formatting and spacing. At the end of the list, include the text '[Download Full List]'. Give only information related to VCs, investors, or startups." },
         ...chatHistory,
       ],
       max_tokens: 500,
